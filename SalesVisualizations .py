639768adec1135a1d8bbc94e5660dc6e7daf73ae
Ryan Stanley <ryan@thestanleys.us>
2024-04-25 11:30:41 -0500

    Added Visualization files

---
+++ SalesVisualizations .py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+# Create array for x- and y- coordinates
+x_axis = []
+salesY = []
+unitsY = []
+
+fig, ax = plt.subplots(2)
+with open('dataSales.csv', mode ='r')as file:
+    csvFile = csv.reader(file)
+    for row in csvFile:
+        if row[0] != 'GameName':
+            unitsY.append(float((row[1])[1:]))
+            salesY.append(float((row[2])[1:]))
+            x_axis.append((row[0])[9:-1])
+
+# Create a line plot
+
+
+ax[0].plot(x_axis, unitsY)
+ax[0].set_xticklabels([])
+fig.set_figwidth(30)
+fig.set_figheight(10)
+ax[0].set_title("Unit Sales")
+ax[0].set_yticklabels(['0', '10M', '15M', '20M', '25M', '30M'])
+
+ax[1].plot(x_axis, salesY, color='green')
+for tick in ax[1].xaxis.get_major_ticks()[1::3]:
+    tick.set_pad(15)
+for tick in ax[1].xaxis.get_major_ticks()[2::3]:
+    tick.set_pad(30)
+fig.set_figwidth(30)
+fig.set_figheight(10)
+ax[1].set_title("All Time Sales")
+ax[1].set_yticklabels(['0', '200M', '400M', '600M', '800M', '1B', '1.2B', '1.4B', '1.6B'])
+fig.suptitle('Pokemon Games\' Sales Data')
+plt.show()
+
---
+++ reviewVisualizations.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+from datetime import datetime
+
+# Create array for x- and y- coordinates
+reviewsX = []
+reviewsY = []
+
+fig, ax = plt.subplots()
+with open('dataReviews.csv', mode ='r')as file:
+    csvFile = csv.reader(file)
+    for row in csvFile:
+        if row[0] != 'gameName':
+            reviewsY.append(float(row[2]))
+            reviewsX.append((row[0])[9:-1])
+
+# Create a line plot
+ax.plot(reviewsX, reviewsY)
+for tick in ax.xaxis.get_major_ticks()[1::3]:
+    tick.set_pad(15)
+for tick in ax.xaxis.get_major_ticks()[2::3]:
+    tick.set_pad(30)
+fig.set_figwidth(30)
+fig.set_figheight(8)
+ax.set_title("Pokemon Games Review Numbers (out of 10)")
+plt.show()
