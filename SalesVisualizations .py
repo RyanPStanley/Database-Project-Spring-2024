b0fc2d07f296e8678ee436b071bd26f893ccd9de
Ryan Stanley <ryan@thestanleys.us>
2024-04-25 15:00:57 -0500

    Made graphs look even better

--- SalesVisualizations .py
+++ SalesVisualizations .py
@@ -3,42 +3,48 @@ import numpy as np
 import csv
 
 # Create array for x- and y- coordinates
-x_axis = []
+years = []
 salesY = []
 unitsY = []
+gameNames = []
 
-fig, ax = plt.subplots(2)
+fig, (ax1, ax2) = plt.subplots(1, 2)
 with open('dataSales.csv', mode ='r')as file:
     csvFile = csv.reader(file)
     for row in csvFile:
         if row[0] != 'GameName':
             unitsY.append(float((row[1])[1:]))
             salesY.append(float((row[2])[1:]))
-            x_axis.append((row[0])[9:-1])
+            gameNames.append((row[0])[9:-1])
 
 # Create a line plot
 
+with open('dataGame.csv', mode = 'r') as file:
+    csvFile = csv.reader(file)
+    for row in csvFile:
+        if row[0] != 'GameName':
+            years.append(row[1])
 
-ax[0].bar(x_axis, unitsY, color='indianred')
-ax[0].set_xticklabels([])
-fig.set_figwidth(30)
+hbar1 = ax1.barh(gameNames, unitsY, color='salmon')
+fig.set_figwidth(40)
 fig.set_figheight(10)
-ax[0].set_title("Unit Sales")
-ax[0].set_yticklabels(['0', '5M', '10M', '15M', '20M', '25M', '30M'])
-ax[0].set_ylabel("Copies sold")
-ax[0].set_facecolor("mistyrose")
-
-ax[1].bar(x_axis, salesY, color='limegreen')
-for tick in ax[1].xaxis.get_major_ticks()[1::3]:
-    tick.set_pad(15)
-for tick in ax[1].xaxis.get_major_ticks()[2::3]:
-    tick.set_pad(30)
-fig.set_figwidth(30)
+ax1.set_title("Unit Sales")
+ax1.set_xticklabels(['0', '5M', '10M', '15M', '20M', '25M', '30M'])
+ax1.set_yticklabels(years)
+ax1.set_xlabel("Copies sold")
+ax1.invert_yaxis()
+ax1.set_facecolor("mistyrose")
+ax1.bar_label(hbar1, labels = gameNames, fontsize='9', label_type = 'center')
+hbar2 = ax2.barh(gameNames, salesY, color='springgreen')
+fig.set_figwidth(40)
 fig.set_figheight(10)
-ax[1].set_title("All Time Sales")
-ax[1].set_yticklabels(['0', '$200M', '$400M', '$600M', '$800M', '$1B', '$1.2B', '$1.4B', '$1.6B'])
-ax[1].set_ylabel("Total Money Made (release to present)")
-ax[1].set_facecolor("honeydew")
+ax2.set_title("All Time Sales")
+ax2.set_xticklabels(['0', '$200M', '$400M', '$600M', '$800M', '$1B', '$1.2B', '$1.4B', '$1.6B'])
+ax2.set_xlabel("Total Money Made (release to present)")
+ax2.set_yticklabels(years)
+ax2.set_facecolor("honeydew")
+ax2.invert_yaxis()
+ax2.bar_label(hbar2, labels = gameNames, fontsize='9', label_type = 'center')
 fig.suptitle('Pokemon Games\' Sales Data')
 plt.show()
 
